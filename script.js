4e0f9756596c0bf0576e2571bdf5a4f0cf7637fc
xlthimolx <Thimo.ludwig@gmx.de>
2025-11-23 01:46:14 +0100

    fade out weicher

--- script.js
+++ script.js
@@ -218,40 +218,42 @@ function stopAudio(forceImmediate = false) {
   const canGainFade = !!gainNode;
   const shouldFade = !forceImmediate;
 
-  if (shouldFade && canGainFade) {
-    const startGain = gainNode.gain.value || volumeLevel || 1;
-    const gainStep = startGain / fadeSteps;
-    fadeIntervalId = setInterval(() => {
-      const next = gainNode.gain.value - gainStep;
-      if (next > 0) {
-        gainNode.gain.value = next;
-      } else {
-        clearInterval(fadeIntervalId);
-        fadeIntervalId = null;
-        gainNode.gain.value = volumeLevel; // reset fuer naechsten Start
-        el.pause();
-        el.currentTime = 0;
-        currentAudio = null;
-      }
-    }, fadeInterval);
+  // Sanfter Fade mit kleinem Rest-Epsilon, dann Stop
+  const stopClean = () => {
+    el.pause();
+    el.currentTime = 0;
+    currentAudio = null;
+    if (gainNode) gainNode.gain.value = volumeLevel;
+  };
+
+  if (shouldFade && canGainFade && audioCtx) {
+    const epsilon = 0.001;
+    const now = audioCtx.currentTime;
+    const startGain = Math.max(gainNode.gain.value || volumeLevel || 1, epsilon);
+    gainNode.gain.cancelScheduledValues(now);
+    gainNode.gain.setValueAtTime(startGain, now);
+    // Exponentieller Ramp klingt am Ende weicher
+    gainNode.gain.exponentialRampToValueAtTime(epsilon, now + fadeOutTime / 1000);
+    setTimeout(() => {
+      stopClean();
+      gainNode.gain.value = volumeLevel;
+    }, fadeOutTime + 50);
   } else if (shouldFade && !IS_IOS) {
+    const epsilon = 0.01;
     const initialVolume = el.volume > 0 ? el.volume : volumeLevel || 1;
-    const volumeStep = initialVolume / fadeSteps;
+    const volumeStep = (initialVolume - epsilon) / fadeSteps;
     fadeIntervalId = setInterval(() => {
-      if (el.volume > volumeStep) {
+      if (el.volume > epsilon + volumeStep) {
         el.volume -= volumeStep;
       } else {
         clearInterval(fadeIntervalId);
         fadeIntervalId = null;
-        el.pause();
-        el.currentTime = 0;
-        currentAudio = null;
+        el.volume = epsilon;
+        stopClean();
       }
     }, fadeInterval);
   } else {
-    el.pause();
-    el.currentTime = 0;
-    currentAudio = null;
+    stopClean();
   }
 }
 
